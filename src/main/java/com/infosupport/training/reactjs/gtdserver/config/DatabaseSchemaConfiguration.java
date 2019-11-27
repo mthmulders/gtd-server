15fdef23f7bf3faaf53a05560b984a72bae2ed8f
Maarten Mulders <maartenm@infosupport.com>
2019-11-27 11:19:50 +0100

    Add version control for database schema

---
+++ src/main/java/com/infosupport/training/reactjs/gtdserver/config/DatabaseSchemaConfiguration.java
@@ -0,0 +1,24 @@
+package com.infosupport.training.reactjs.gtdserver.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@AllArgsConstructor
+@Configuration
+public class DatabaseSchemaConfiguration {
+    private final DataSource dataSource;
+
+    @Bean
+    public SpringLiquibase databaseSchema() {
+        final SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db-changeLog.xml");
+        liquibase.setDataSource(dataSource);
+
+        return liquibase;
+    }
+}
